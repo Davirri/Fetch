7d3d5fc3a90234132c0768405cff5dcd0f48cc94
Davirri <167114229+Davirri@users.noreply.github.com>
2024-07-23 09:07:09 +0200

    change async

--- async/vite-project/src/App.jsx
+++ async/vite-project/src/App.jsx
@@ -1,16 +1,27 @@
-
 import './App.css';
 import FakeStoreGallery from './Components/Fake';
 import PokeGallery from './Components/Poke';
 import RandomUserGallery from './Components/Random';
+import { useState } from 'react';
 
 const App = () => {
+  const [visible, setVisible] = useState('fake');
+
   return (
-    <div>
+    <div className='Container'>
       <h1>API Galleries</h1>
-      <FakeStoreGallery />
-      <PokeGallery />
-      <RandomUserGallery />
+
+      <div>
+        <button onClick={() => setVisible('fake')}>Show Fake Store</button>
+        <button onClick={() => setVisible('poke')}>Show Pokemon</button>
+        <button onClick={() => setVisible('random')}>Show Random User</button>
+      </div>
+
+      <div>
+        {visible === 'fake' && <FakeStoreGallery />}
+        {visible === 'poke' && <PokeGallery />}
+        {visible === 'random' && <RandomUserGallery />}
+      </div>
     </div>
   );
 };
